14bc24e4f3511c6e0c6cc07bec8444415e5cf52f
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-05-15 16:26:32 -0700

    Script to handle cache upload.
    [ci skip]

--- UploadCachedBuildDir.sh
+++ UploadCachedBuildDir.sh
@@ -1,19 +1,22 @@
 set -e
 
-BUILD_DIR=$(pwd)
-echo $BUILD_DIR
+if [ $(git branch | grep '*' | sed 's/^* //') != "master" ]; then 
+  echo "---- Not caching build directory. Current branch is not master."
+  return
+fi
 cd $TRAVIS_BUILD_DIR
-git fetch origin master:master
-git checkout 64dca7cd40275491f09f206d849d80cd6ef381a3
 MASTERTIP=$(git log -n1 --format="%H")
-echo $VERSION
-cd $BUILD_DIR
-make -j$NPROC
-rm -rf Bindings
-cd ..
+cd ${OPENSIM_BUILD_DIR}/..
+echo "---- Compressing build directory into a tarball."
 tar -czf $TARBALL opensim-core-build
+echo "---- Splitting tarball into smaller pieces for upload."
 split -b 200m $TARBALL $TARBALL
 URL="https://api.bintray.com/content/opensim/opensim-core/${PACKAGENAME}/${MASTERTIP}/${PACKAGENAME}/${MASTERTIP}"
-ls ${TARBALL}a* | while read f; do curl -T $f -uklshrinidhi:440061321dba00a68210b482261154ea58d03f00 ${URL}/${f}; done
+PIECES=$(ls ${TARBALL}a*)
+for piece in PIECES; do 
+  echo "---- Uploading piece \'${piece}\' to \'opensim/opensim-core/${PACKAGENAME}/${MASTERTIP}\'"
+  curl -T $piece -uklshrinidhi:440061321dba00a68210b482261154ea58d03f00 ${URL}/${piece} 
+done
 URL="https://api.bintray.com/content/opensim/opensim-core/${PACKAGENAME}/${VERSION}/publish"
+echo "---- Publishing uploaded build directory."
 curl -X POST -uklshrinidhi:440061321dba00a68210b482261154ea58d03f00 $URL
