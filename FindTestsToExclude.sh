4a65d4852fc94b362bd1fc6a91fc12f05dbf499e
Shrinidhi KL <klshrinidhi@users.noreply.github.com>
2016-05-15 22:03:37 -0700

    Remove -executable. Not supported on osx. [skip appveyor]

--- FindTestsToExclude.sh
+++ FindTestsToExclude.sh
@@ -5,11 +5,13 @@ MINS_SINCE_BUILD_START=$(( (($(date +%s) - $BUILD_START_TIMESTAMP) / 60) + 1 ))
 ALLTESTS=$(ctest --show-only | grep 'Test #' | sed 's/^.*: //')
 COUNTER=0
 for test in $ALLTESTS; do
-  findres=$(find . -name "*$test" -executable -mmin +$MINS_SINCE_BUILD_START)
+  findres=$(find . -name "*$test" -mmin +$MINS_SINCE_BUILD_START)
   if [ "$findres" != "" ]; then
     TESTS_TO_EXCLUDE="$TESTS_TO_EXCLUDE|$test"
     echo "---- Excluding test $test."
     COUNTER=$(($COUNTER + 1))
+  else
+    echo "---- Cannot exclude $test."
   fi
 done
 if [[ $COUNTER == 0 ]]; then
